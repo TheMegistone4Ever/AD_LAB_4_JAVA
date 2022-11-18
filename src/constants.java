fb4419e6da3d3c9ffac7b6615365e709b777f672
TheMegistone4Ever <zeusmobilenick@gmail.com>
2022-11-18 23:52:33 +0200

    DO MUCH

--- src/constants.java
+++ src/constants.java
@@ -1,10 +1,13 @@
 public class constants {
     public static final int ITERATIONS_COUNT = 1000;
-    public static final int ITERATIONS_PER_STEP = 100;
+    public static final int ITERATIONS_PER_STEP = 10;
+
     public static final int VERTEX_COUNT = 300;
     public static final int MIN_VERTEX_DEGREE = 1;
     public static final int MAX_VERTEX_DEGREE = 30;
+
+    public static final int TOTAL_BEES_COUNT = 60;
     public static final int EMPLOYED_BEES_COUNT = 5;
-    public static final int ONLOOKER_BEES_COUNT = 25;
-    public static final int PALETTE_SIZE = 10000;
+    public static final int ONLOOKER_BEES_COUNT = TOTAL_BEES_COUNT - EMPLOYED_BEES_COUNT;
+    public static final int PALETTE_SIZE = VERTEX_COUNT;
 }
